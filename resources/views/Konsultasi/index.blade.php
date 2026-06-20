04907337df1470066b7429cb13e9ff781662e2df
rizkiard1-kyy <rizkiardi405@gmail.com>
2026-06-20 21:03:40 +0700

    fix: memperbaiki kesalahan router create konsultasi yang nyambung ke surat permohonan

--- resources/views/Konsultasi/index.blade.php
+++ resources/views/Konsultasi/index.blade.php
@@ -24,7 +24,7 @@
         <h1>Konsultasi Akademik</h1>
 
 @if(auth()->user()->isMahasiswa())
-    <a href="{{ route('surat_keterangan.create') }}" class="btn-primary">
+    <a href="{{ route('konsultasi.create') }}" class="btn-primary">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
             Tambah Surat
         </a>
