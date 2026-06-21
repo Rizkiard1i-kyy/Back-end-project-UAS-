69177b9e961f64c9e6055e2b63154f641183c151
rizkiard1-kyy <rizkiardi405@gmail.com>
2026-06-21 14:16:58 +0700

    refactor: mengubah nama nama dosen di seeder dan delet file edit konsultasi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -70,8 +70,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Pengguna::create([
-            'nama'     => 'yanto',
-            'email'    => 'yanto@gmail',
+            'nama'     => 'AGUS BUDI DHARMAWAN (S.Kom, M.T., M.Sc.)',
+            'email'    => 'agus@gmail',
             'nim'      => '',
             'password' => Hash::make('12345678'),
             'role'     => 'dosen',
@@ -79,8 +79,8 @@ class DatabaseSeeder extends Seeder
 
         
         Pengguna::create([
-            'nama'     => 'dewi ',
-            'email'    => 'dewi@gmail',
+            'nama'     => 'BAGUS MULYAWAN (Dr., S.Kom., M.M.)',
+            'email'    => 'bagus@gmail',
             'nim'      => '',
             'password' => Hash::make('12345678'),
             'role'     => 'dosen',
@@ -88,8 +88,8 @@ class DatabaseSeeder extends Seeder
 
         
         Pengguna::create([
-            'nama'     => 'ajeng',
-            'email'    => 'ajeng@gmail',
+            'nama'     => 'CHAIRISNI LUBIS (Dra., M.Kom. )',
+            'email'    => 'chatrisni@gmail',
             'nim'      => '',
             'password' => Hash::make('12345678'),
             'role'     => 'dosen',
@@ -97,13 +97,140 @@ class DatabaseSeeder extends Seeder
 
         
         Pengguna::create([
-            'nama'     => 'supriadi',
-            'email'    => 'supriadi@gmail',
+            'nama'     => 'DARIUS ANDANA HARIS (S.KOM., M.T.I.)',
+            'email'    => 'darius@gmail',
             'nim'      => '',
             'password' => Hash::make('12345678'),
             'role'     => 'dosen',
         ]);
 
+        Pengguna::create([
+            'nama'     => 'DEDI TRISNAWARMAN (S.Si., M.Kom., Dr.)',
+            'email'    => 'dedi@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+
+        Pengguna::create([
+            'nama'     => 'DESI ARISANDI (S.Kom., M.T.I.)',
+            'email'    => 'desi@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+
+        Pengguna::create([
+            'nama'     => 'DYAH ERNY HERWINDIATI (Prof., Ir., M.Si, Dr.)',
+            'email'    => 'dyah@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+
+        Pengguna::create([
+            'nama'     => 'IRVAN LEWENUSA (S.Kom., M.Kom.)',
+            'email'    => 'irvan@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+
+        Pengguna::create([
+            'nama'     => 'JANSON HENDRYLI (S. Kom. M.Kom.)',
+            'email'    => 'janson@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+
+        Pengguna::create([
+            'nama'     => 'JAP TJI BENG (Ir., MMSI., M.Psi., Ph.D., P.E., M.ASCE)',
+            'email'    => 'jap@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+
+        Pengguna::create([
+            'nama'     => 'JEANNY PRAGANTHA (Ir., M.Eng)',
+            'email'    => 'jenny@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+
+        Pengguna::create([
+            'nama'     => 'LELY HIRYANTO (ST., M.Sc.,Ph.D.)',
+            'email'    => 'lely@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+        
+        Pengguna::create([
+            'nama'     => 'LINA (Prof. S.T., M.Kom., Ph.D.)',
+            'email'    => 'lina@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+        
+        Pengguna::create([
+            'nama'     => 'MANATAP DOLOK LAURO (S.Kom., M.M.S.I.)',
+            'email'    => 'manatap@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+        
+        Pengguna::create([
+            'nama'     => 'NOVARIO JAYA PERDANA (S.Kom., M.T.)',
+            'email'    => 'novario@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+        
+        Pengguna::create([
+            'nama'     => 'TENY HANDHAYANI (S.Kom., M.Kom., Ph.D.)',
+            'email'    => 'teny @gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+        
+        Pengguna::create([
+            'nama'     => 'TONY (S.Kom., M.Kom., Ph. D.)',
+            'email'    => 'tony@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+
+        Pengguna::create([
+            'nama'     => 'TRI SUTRISNO (S.Si., M.Sc.)',
+            'email'    => 'tri@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+        
+        Pengguna::create([
+            'nama'     => 'VINY CHRISTANTI MAWARDI (S.Kom., M.Kom.)',
+            'email'    => 'viny@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
+
+        Pengguna::create([
+            'nama'     => 'WASINO (S.Kom., M.Kom.,Dr.)',
+            'email'    => 'wasino@gmail',
+            'nim'      => '',
+            'password' => Hash::make('12345678'),
+            'role'     => 'dosen',
+        ]);
         $this->call([
             TagSeeder::class,
             MataKuliahSeeder::class,
